5c75c741513806149ec1331cd76af2344d8fa6e1
q-0q <jrwthrs@gmail.com>
2023-10-23 09:06:07 -0400

    screen fade and in/out room force walk

--- Player/ScreenFade.gd
+++ Player/ScreenFade.gd
@@ -1,4 +1,9 @@
 extends Node
 
-func fade():
-	$AnimationPlayer.play("fade", -1, 2)
+func fade_out():
+	print("fade out")
+	$AnimationPlayer.play("fade_out")
+	
+func fade_in():
+	print("fade in")
+	$AnimationPlayer.play("fade_in")
--- SceneManager.gd
+++ SceneManager.gd
@@ -4,6 +4,9 @@ extends Node
 var player : Player
 var current_scene
 
+var current_transition_origin : Doorway
+var current_player : Player
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var root = get_tree().root
@@ -16,14 +19,24 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func fade_out():
+	player.get_node("ScreenFade").fade_out()
+	player.FSM.force_walk_for_time(0.33)
 
 func on_doorway_entered(origin : Doorway, player : Player):
-	call_deferred("switch_scene_via_doorway", origin, player)
+	current_transition_origin = origin
+	current_player = player
+	$AnimationPlayer.play("doorway_transition")
 
-func switch_scene_via_doorway(origin : Doorway, player : Player):
-	player.get_parent().remove_child(player)
-	var s = load(origin.dest_scene_path)
-	var dest_doorway_name = origin.dest_door_name
+func d_on_doorway_entered():
+	call_deferred("switch_scene_via_doorway")
+	
+func switch_scene_via_doorway():
+	player.FSM.force_walk_for_time(0)
+	current_player.get_parent().remove_child(current_player)
+	var s = load(current_transition_origin.dest_scene_path)
+	var dest_doorway_name = current_transition_origin.dest_door_name
 	current_scene.free()
 	current_scene = s.instantiate()
 	get_tree().root.add_child(current_scene)
@@ -32,10 +45,12 @@ func switch_scene_via_doorway(origin : Doorway, player : Player):
 	var dest_doorway = current_scene.get_node(dest_doorway_path)
 	player.position = dest_doorway.position
 	player.can_enter_doors = false
-	player.get_node("ScreenFade").fade()
-	player.FSM.force_walk_for_time(0.33)
 	set_camera_limits()
 	get_tree().current_scene.add_child(player)
+	
+func fade_in():
+	player.FSM.force_walk_for_time(0.3)
+	player.get_node("ScreenFade").fade_in()
 
 func set_camera_limits():
 	var cam : Camera2D = player.get_node("Camera2D")
